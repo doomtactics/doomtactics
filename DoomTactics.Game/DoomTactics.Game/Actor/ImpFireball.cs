c689eb1b19a5ab82dfbeba09e789f8e761f532a8
michael.reid <mreid@mreid>
2012-10-03 14:07:31 -0700

    Misisng file

---
+++ DoomTactics.Game/DoomTactics.Game/Actor/ImpFireball.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DoomTactics
+{
+    public class ImpFireball : ActorBase
+    {
+        private static long ballNum = 0;
+
+        public ImpFireball(string id) : base(id + ballNum++)
+        {
+            CurrentAnimation = ActorAnimationManager.Make("impidle", "testimp");
+            SpriteSheet = SpriteSheetFactory.CreateSpriteSheet(ActorType.ImpFireball);
+        }
+    }
+}
