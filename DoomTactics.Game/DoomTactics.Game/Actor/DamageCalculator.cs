4b0e35886aac3031cd45345e7635721556fc9e6e
michael.reid <mreid@mreid>
2012-10-20 16:17:53 -0700

    Add basic damage dealing stuff

---
+++ DoomTactics.Game/DoomTactics.Game/Actor/DamageCalculator.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DoomTactics
+{
+
+
+    public class DamageCalculator
+    {
+        public int TotalDamage;        
+
+        public DamageCalculator()
+        {
+            
+        }
+
+        public static 
+    }
+}
